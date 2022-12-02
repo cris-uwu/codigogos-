da2675c025bf24b5ec0ddd81d188c71345f03345
cris <cristian1322134232@uptecamac.edu.mx>
2022-12-02 01:43:05 -0600

    codigos

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace codigo_arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese los numeros del vector");
+
+        }
+    }
+}
